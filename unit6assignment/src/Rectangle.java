76fc023a0089e0d5a7ef705c596d1ac56cd7092d
Deacon Sham <102269921+lolambition@users.noreply.github.com>
2023-04-27 13:43:19 -0400

    Update Rectangle.java

--- unit6assignment/src/Rectangle.java
+++ unit6assignment/src/Rectangle.java
@@ -1,30 +1,30 @@
-class Rectangle { //1
+class Rectangle { //class 1
     private int left, bot, width, height;
-    public Rectangle () { //2
+    public Rectangle () { //method 2
         this.left = 0;
         this.bot = 0;
         this.width = 0;
         this.height = 0;
     }
-    public Rectangle (int x, int y, int w, int h) { //3
+    public Rectangle (int x, int y, int w, int h) { //method 3
         this.left = x;
         this.bot = y;
         this.width = Math.max(0,w);
         this.height = Math.max(0, h);
     }
-    public void set (int x, int y, int w, int h) { //4
+    public void set (int x, int y, int w, int h) { //method 4
         this.left = x;
         this.bot = y;
         this.width = Math.max(0,w);
         this.height = Math.max(0, h);
     }
-    public String toString() { //5
+    public String toString() { //method 5
         return "base: (" + this.left + "," + this.bot + ") w:" + this.width + " h:" + this.height; 
     }
-    public int area() { //6
+    public int area() { //method 6
         return this.width * this.height;
     }
-    public int perimeter() { //7
+    public int perimeter() { //method 7
         if (this.height == 0 && this.width == 0) {
             return 0;
         }
@@ -38,10 +38,10 @@ class Rectangle { //1
             return (2 * this.width) + (2 * this.height);
         }
     }
-    public boolean contains (Rectangle other) { //8
+    public boolean contains (Rectangle other) { //method 8
         return this.left <= other.left && this.bot <= other.bot && this.left + this.width >= other.left + other.width && this.bot + this.height >= other.bot + other.height;
         }
-    public static Rectangle intersection (Rectangle r1, Rectangle r2) { //9
+    public static Rectangle intersection (Rectangle r1, Rectangle r2) { //method 9
         int lefties = Math.max(r1.getLeft(), r2.getLeft());
         int botsies = Math.max(r1.getBot(), r2.getBot());
         int rightsies = Math.min(r1.getLeft() + r1.getWidth(), r2.getLeft() + r2.getWidth());
@@ -53,10 +53,14 @@ class Rectangle { //1
             return new Rectangle(lefties, botsies, rightsies - lefties, topsies - botsies);
         }
     }
-    public static int totalPerimeter (Rectangle r1, Rectangle r2) {
-        return (r1.perimeter() + r2.perimeter()) - intersection(r1, r2).perimeter();
+    public static int totalPerimeter (Rectangle r1, Rectangle r2) { //method 10
+        Rectangle combined = (intersection(r1, r2));
+		if(combined.getWidth() == 0 || combined.getHeight() == 0) {
+			combined.set(combined.getLeft(), combined.getBot(), combined.getHeight() * 2, combined.getWidth() * 2);
+        }
+        return r1.perimeter() + r2.perimeter() - combined.perimeter();
     }
-    //get methods to execute methods 9 and 10
+    //additional get methods to execute methods 9 and 10
     public int getLeft() {
         return left;
     }
