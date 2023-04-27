04c1921729e5b176eaad685a7cfbe666e7aca1ed
Deacon Sham <102269921+lolambition@users.noreply.github.com>
2023-04-27 13:45:33 -0400

    Delete unit6assignment directory

--- unit6assignment/src/App.java
+++
@@ -1,9 +0,0 @@
-import java.io.*;
-import java.util.*;
-public class App {
-    public static Scanner ui = new Scanner (System.in);
-    public static void main(String[] args) throws Exception {
-        Rectangle r = new Rectangle();
-        Rectangle a = new Rectangle();
-    }
-}
